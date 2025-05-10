163f78d45c899eb5f19f8ead682dbf9b0afd7c3c
Natã Hermes <nhermesdev@gmail.com>
2025-05-10 07:05:37 -0300

    feat: implement GitHub authentication flow and update environment configuration

---
+++ apps/web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,33 @@
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+import { signInWithGithub } from '@/http/auth/sign-in-with-github'
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+
+  const code = searchParams.get('code')
+
+  const cookiesStore = await cookies()
+
+  if (!code) {
+    return NextResponse.json(
+      { message: 'Github OAuth  code was not found.' },
+      { status: 400 },
+    )
+  }
+
+  const { token } = await signInWithGithub({ code })
+
+  cookiesStore.set('token', token, {
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7, // 7days
+  })
+
+  const redirectUrl = request.nextUrl.clone()
+
+  redirectUrl.pathname = '/'
+  redirectUrl.search = ''
+
+  return NextResponse.redirect(redirectUrl)
+}
