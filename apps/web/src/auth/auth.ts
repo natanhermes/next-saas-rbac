77c1fd94af89f1f1bebf3db12f61de9ef740165a
Natã Hermes <nhermesdev@gmail.com>
2025-05-15 15:13:22 -0300

    feat: enhance authentication flow with organization-based controls management

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,12 +1,47 @@
+import { defineAbilityFor } from '@saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import { getProfile } from '@/http/auth/get-profile'
+import { getMembership } from '@/http/membership/get-membership'
 
 export async function isAuthenticated() {
   const cookiesStore = await cookies()
 
   return !!cookiesStore.get('token')?.value
+}
+
+export async function getCurrentOrg() {
+  const cookiesStore = await cookies()
+
+  return cookiesStore.get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const org = await getCurrentOrg()
+
+  if (!org) {
+    return null
+  }
+
+  const { membership } = await getMembership(org)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
@@ -19,8 +54,6 @@ export async function auth() {
 
   try {
     const { user } = await getProfile()
-
-    console.log(user)
 
     return { user }
   } catch {}
--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -1,7 +1,7 @@
 import { ChevronsUpDown, PlusCircle } from 'lucide-react'
-import { cookies } from 'next/headers'
 import Link from 'next/link'
 
+import { getCurrentOrg } from '@/auth/auth'
 import { getOrganizations } from '@/http/organizations/get-organizations'
 
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
@@ -18,9 +18,7 @@ import {
 export async function OrganizationSwitcher() {
   const { organizations } = await getOrganizations()
 
-  const cookiesStore = await cookies()
-
-  const currentOrg = cookiesStore.get('org')?.value
+  const currentOrg = await getCurrentOrg()
 
   const currentOrganization = organizations.find(
     (org) => org.slug === currentOrg,
@@ -29,7 +27,6 @@ export async function OrganizationSwitcher() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus-visible:ring-primary flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2">
-        <span className="text-muted-foreground">Select organization</span>
         {currentOrganization ? (
           <>
             <Avatar className="mr-2 size-4">
